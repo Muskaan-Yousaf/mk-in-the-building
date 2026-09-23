d90e7606b87f14de510566b90fdae865ad814d65
Muskaan-Yousaf <you124@stge.org.uk>
2026-09-23 13:18:02 +0000

    this is so difficult bruh ;-;

---
+++ maxAndMinTasks.py
@@ -0,0 +1,49 @@
+# scores = [48, 72, 65, 91, 54, 83]
+
+# maxScore = scores[0]
+
+# for x in range(1, len(scores)):
+#     print("The current max score is " + str(maxScore))
+#     if scores[x] > maxScore:
+#         maxScore = scores[x]
+
+# print(maxScore)
+
+# temperatures = [7, 3, -2, 5, 0, -6, 4]
+
+# minTemp = temperatures[0]
+
+# for x in range(1, len(temperatures)):
+#     print("The current min temp is " + str(minTemp))
+#     if temperatures[x] < minTemp:
+#         minTemp = temperatures[x]
+
+# numbers = [23, 8, 41, 16, 5, 37, 12]
+
+# minNumber = numbers[0]
+# maxNumber = numbers[0]
+
+# for x in range(1, len(numbers)):
+#     if numbers[x] < minNumber:
+#         minNumber = numbers[x]
+#     if numbers [x] > maxNumber:
+#         maxNumber = numbers[x]
+
+# print("The current min number is " + str(minNumber) + " and the current max number is " + str(maxNumber))
+
+# scores = [56, 71, 88, 95, 79]
+
+# maxScore = scores[0]
+# index = 0
+
+# for x in range(1, len(scores)):
+#     if scores[x] > maxScore:
+#         maxScore = scores[x]
+#         index = x 
+
+# print("Highest score: " + str(maxScore))
+# print("Found at index: " + str(index + 1))
+
+scores = [38, 39, 47, 58, 62, 67, 79, 94]
+highScore = 0
+lowScore = 0
